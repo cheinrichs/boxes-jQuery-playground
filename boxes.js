9581c5bb16ad751d5d429a8ef47d04a44bef87f7
Cooper Heinrichs <cooper.heinrichs@gmail.com>
2016-02-16 13:41:12 -0700

    completed exercise 2

--- boxes.js
+++ boxes.js
@@ -1,1 +1,9 @@
-console.log("hello world");
+$(function(){
+  $('#secretBox').css({"background-color": "white"}).append("<h1>Secret Box!</h1>");
+  $('#row1').children().removeClass('boxType1').removeClass('boxType2').addClass('boxType3');
+  $( "#row4 :nth-child(4)" ).hide();
+  $( ".boxType1" ).css({"background-color": "white"});
+  $('#row2 :nth-child(1)' ).removeClass('boxType2').removeClass('box');
+  $('#row2 :nth-child(2)' ).removeClass('boxType3').removeClass('box');
+  $('#container').children().children().not('#secretBox').css({"width":"2px"});
+});
