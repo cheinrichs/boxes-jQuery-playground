f048eafc29ffaaaa3552ca0f55ba761c0cbb8fe5
Cooper Heinrichs <cooper.heinrichs@gmail.com>
2016-02-16 13:36:55 -0700

    finished adding an alert on page ready

--- boxes.js
+++ boxes.js
@@ -1,13 +1,3 @@
 $(function(){
-  //alert( "ready!" );
-  $('#secretBox').css({"background-color": "white"}).append("<h1>Secret Box!</h1>");
-  $('#row1').children().removeClass('boxType1').removeClass('boxType2').addClass('boxType3');
-  $( "#row4 :nth-child(4)" ).hide();
-  $( ".boxType1" ).css({"background-color": "white"});
-
-  $('#row2 :nth-child(1)' ).removeClass('boxType2').removeClass('box');
-  $('#row2 :nth-child(2)' ).removeClass('boxType3').removeClass('box');
-
-  $('#container').children().children().not('#secretBox').css({"width":"2px"});
-
-});
+  alert( "ready!" );
+})
